72dc2bbb21577e2da92298462164e123357da91f
MarcosCDV <119008159+MarcosCDV@users.noreply.github.com>
2023-05-09 18:30:27 -0300

    Add files via upload

---
+++ dotnet-exam-main/src/DotnetExam.Entities/Docente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetExam.Entities
+{
+    public class Docente:Persona
+    {
+        public int DocenteId { get; set; }
+        public int Antiguedad { get; set; }
+
+        public Docente(int id, string nombre, string apellido)
+
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+            DocenteId = id;
+
+        }
+
+        public Docente(int id, string nombre, string apellido, int antiguedad)
+       
+        {
+            Id = id;    
+            Nombre = nombre;
+            Apellido = apellido;
+            DocenteId = id;
+            Antiguedad = antiguedad;
+        }
+
+        public override string NombreCompleto()
+        {
+            return $"{Nombre} {Apellido}";
+        }
+    }
+}
